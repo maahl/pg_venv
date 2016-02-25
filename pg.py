f14104c0ba0fcbc66797514209e14b579a460afb
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2016-02-25 13:40:43 +0100

    Implement configure action on postgresql

--- pg.py
+++ pg.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
 import sys
 
 
+LOG_PREFIX = 'pg_wrapper: '
 USAGE = '''
 Usage:
     pg.py <action> [args]
 
 Actions:
-    help, h: display this help text
+    help, h:
+        Display this help text
+    configure, c:
+        Run `.configure` in postgresql source dir Set PG_DIR environment
+        variable to the location of postgresql source
+        dir.
+        Set PG_CONFIGURE_OPTIONS for options to be used by the configure script.
 '''
 
 
-def usage():
-    print(USAGE)
+def configure():
+    '''
+    Run `./configure` in postgresql dir
+
+    Uses env var PG_DIR for location of postgresql source, and
+    PG_CONFIGURE_OPTIONS for options.
+    '''
+    try:
+        pg_dir = os.environ['PG_DIR']
+    except KeyError:
+        print('Please set environment variable PG_DIR to point to your '
+              'postgresql source dir.')
+
+    pg_configure_options = os.environ.get('PG_CONFIGURE_OPTIONS', '')
+
+    cmd = 'cd {} && ./configure {}'.format(pg_dir, pg_configure_options)
+    execute_cmd(cmd)
 
 
 def execute_action(action, action_args):
@@ -41,12 +65,50 @@ def execute_action(action, action_args):
         exit(-1)
 
 
+def execute_cmd(cmd):
+    '''
+    Execute a shell command, binding stdin and stdout to this process' stdin
+    and stdout
+    '''
+    log('executing `{}`'.format(cmd))
+    process = subprocess.Popen(cmd, shell=True)
+    process.communicate()
+
+    if process.returncode == 0:
+        log('command succesfully executed', 'success')
+    else:
+        log('command failed', 'error')
+        log('command used: {}'.format(cmd), 'error')
+
+
+def log(message, message_type='log'):
+    '''
+    Print a message to stdout
+    message_type changes the color in which the message is displayed
+    Possible message_type values: log, error, success
+    '''
+    if message_type == 'log':
+        # don't change color
+        pass
+    elif message_type == 'error':
+        message = '\033[0;31m' + message + '\033[0;m'
+    elif message_type == 'success':
+        message = '\033[0;32m' + message + '\033[0;m'
+
+    print(LOG_PREFIX + message)
+
+
+def usage():
+    print(USAGE)
+
 
 ACTIONS = {
+    'configure': configure,
     'help': usage,
 }
 
 ALIASES = {
+    'c': 'configure',
     'h': 'help',
 }
 
